b7811533db7788e67485f9c121500802a7a38003
Alexei Karamzin <1999aleks_1999@mail.ru>
2023-05-21 19:52:12 +0300

    test styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,20 @@ import styled from "styled-components";
 function App() {
   return (
     <div className="App">
+
+        <Menu>
+            <ul>
+                <li><a href="">menu item 1</a></li>
+                <li><a href="">menu item 2</a></li>
+                <li><a href="">menu item 3</a></li>
+            </ul>
+        </Menu>
+
         <Box>
-            <StyledBtn>hello</StyledBtn>
+            <StyledBtn as={Link}>Link</StyledBtn>
+            <StyledBtn as="a">link</StyledBtn>
+            <StyledBtn>Btn</StyledBtn>
+            <SuperButton>Super Btn</SuperButton>
         </Box>
     </div>
   );
@@ -18,12 +30,45 @@ const StyledBtn = styled.button`
   border: none;
   background-color: rgba(243, 97, 97, 0.62);
   padding: 10px 20px;
-  border-radius: 5px;
   color: snow;
+
+  &:hover {
+    background-color: #2eeccc;
+  }
+
+  &:last-child {
+    background-color: #009427;
+  }
 `
+
+const SuperButton = styled(StyledBtn)`
+  border-radius: 10px;
+`
+
+const Link = styled.a`
+  color: aqua;
+  font-size: 2rem;
+  font-weight: bold;
+  background-color: #000000;
+  padding: 0;
+`
+
 const Box = styled.div`
   display: flex;
   height: 100vh;
   justify-content: center;
   align-items: center;
+  gap: 20px;
+  
+  button {
+    cursor: pointer;
+  }
+  
+  ${Link} {
+    cursor: zoom-in;
+  }
+`
+
+const Menu = styled.nav`
+    
 `
